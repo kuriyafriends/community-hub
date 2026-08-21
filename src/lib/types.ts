425cf2f64ca94097d5ac106ec0c05e2a4c38d092
kuriyafriends <kuriyabento@gmail.com>
2026-08-21 17:57:28 +0200

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,6 @@
 export interface Profile {
   id: string
+  user_id: string
   email: string
   display_name: string
   avatar_url: string | null
@@ -18,6 +19,7 @@ export interface Category {
 
 export interface Listing {
   id: string
+  reference_code: string | null
   user_id: string
   category_id: string
   title: string
@@ -51,6 +53,7 @@ export interface ListingComment {
 
 export interface Discussion {
   id: string
+  reference_code: string | null
   user_id: string
   category_id: string
   title: string
